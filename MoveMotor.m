504756bd9275f18eb3db1a047b85a0d463bdb3f9
AlexanderBerny <108438660+AlexanderBerny@users.noreply.github.com>
2023-10-12 22:35:27 +0000

    modified:   MoveMotor.m

--- MoveMotor.m
+++ MoveMotor.m
@@ -1,16 +1,39 @@
-i = 0;
-for i = 1:30
-   touch = myLego.TouchPressed(1);
-   touch2 = myLego.TouchPressed(2);
-    if touch == 1 
-        myLego.MoveMotor('A',-50);
-        i = i + 1;
-    else if touch2 == 1
-        myLego.MoveMotor('A',50);
-        i = i + 1;
-    else
-        myLego.MoveMotor('A', 0)
-        i = i + 1;
-    end
-    end   
+global key
+InitKeyboard();
+
+while 1
+    pause(0.1);
+        switch key
+
+            case 'w'
+                myLego.MoveMotor('A', -50);
+                pause(0.1);
+                myLego.StopMotor('A');
+
+            case 's'
+                myLego.MoveMotor('A', 50);
+                pause(0.1);
+                myLego.StopMotor('A');
+
+            case 'a'
+                myLego.MoveMotor('B', -50);
+                pause(0.1);
+                myLego.StopMotor('B');
+
+            case 'd'
+                myLego.MoveMotor('B', 50);
+                pause(0.1);
+                myLego.StopMotor('B');
+
+            case 'uparrow'
+                myLego.MoveMotor('C', -50);
+                pause(0.1);
+                myLego.StopMotor('C');
+
+            case 'downarrow'
+                myLego.MoveMotor('C', -50);
+                pause(0.1);
+                myLego.StopMotor('C');  
+
+        end
 end
