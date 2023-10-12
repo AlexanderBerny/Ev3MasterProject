07ace55d2ba763d7ff9f46f5bf2a7505e5f7aff5
AlexanderBerny <108438660+AlexanderBerny@users.noreply.github.com>
2023-10-12 22:13:46 +0000

    modified:   GyroSensor.m

--- GyroSensor.m
+++ GyroSensor.m
@@ -1,3 +1,11 @@
-gS = gyroSensor(myev3,1); 
-angle = readRotationAngle(gS);
-display(angle);
+brick.GyroCalibrate(1);
+pause('on');
+
+for i = 0:10
+    angle = brick.GyroAngle(1);
+    display(angle);
+    %angleRate = brick.GyroRate(1);
+    %display(angleRate);
+    pause(0.5);
+    i = i + 0.5;
+end
