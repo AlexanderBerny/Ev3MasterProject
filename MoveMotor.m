a3469505d48a573d8fe3011ffe20699218abbcea
AlexanderBerny <108438660+AlexanderBerny@users.noreply.github.com>
2023-10-26 22:00:47 +0000

    modified:   MoveMotor.m

--- MoveMotor.m
+++ MoveMotor.m
@@ -6,33 +6,34 @@ while 1
         switch key
 
             case 'w'
-                myLego.MoveMotor('A', -50);
-                pause(0.1);
-                myLego.StopMotor('A');
+                myLego.MoveMotor('AB', -75);
+                pause(0.3);
+                myLego.StopMotor('AB');
 
             case 's'
-                myLego.MoveMotor('A', 50);
-                pause(0.1);
-                myLego.StopMotor('A');
+                myLego.MoveMotor('AB', 75);
+                pause(0.3);
+                myLego.StopMotor('AB');
 
             case 'a'
-                myLego.MoveMotor('B', -50);
-                pause(0.1);
+                myLego.MoveMotor('B', 85);
+                pause(0.4);
                 myLego.StopMotor('B');
+                
 
             case 'd'
-                myLego.MoveMotor('B', 50);
-                pause(0.1);
-                myLego.StopMotor('B');
+                myLego.MoveMotor('A', 85);
+                pause(0.4);
+                myLego.StopMotor('A');
 
             case 'uparrow'
-                myLego.MoveMotor('C', -50);
-                pause(0.1);
+                myLego.MoveMotor('C', -100);
+                pause(0.4);
                 myLego.StopMotor('C');
 
             case 'downarrow'
-                myLego.MoveMotor('C', -50);
-                pause(0.1);
+                myLego.MoveMotor('C', 100);
+                pause(0.4);
                 myLego.StopMotor('C');  
 
         end
